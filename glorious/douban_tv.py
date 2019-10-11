c8bced97c0914ee4c991172e4398f1a8504e1a8b
yueyeKIDL <ace1412kid@163.com>
2019-10-11 16:45:40 +0800

    调整result变量

--- glorious/douban_tv.py
+++ glorious/douban_tv.py
@@ -1,14 +1,13 @@
 import csv
-import sys
 import getopt
-
+import sys
 import time
 from collections import defaultdict
 
 import requests
 from bs4 import BeautifulSoup
 
-from urllib.parse import quote, unquote, urlencode
+# from urllib.parse import quote, unquote, urlencode
 
 # url解码
 # decode_data = unquote('%E7%BE%8E%E5%89%A7')
@@ -21,14 +20,13 @@ headers = {
     "Referer": "https://movie.douban.com/",
     "Upgrade-Insecure-Requests": "1",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
-    "Cookie": 'Cookie:bid=R_NjGlmHzLc; ll="108289"; __utmv=30149280.7162; douban-profile-remind=1; __guid=223695111.2801261442483869700.1555121081221.0579; __yadk_uid=su0b7K9g36n02VtPodU6bxjSmrqQNeVi; _vwo_uuid_v2=DA75957D9968A846830D702056248817A|624640ba21d7d6658c71f663d419f44e; douban-fav-remind=1; gr_user_id=d3b07b2b-f556-4601-b7a2-37f2cbe0d6fc; __gads=ID=af43163fe1df4ca9:T=1557648109:S=ALNI_MYSMPOl2Hkvn60cUHrt3jzyod7RSg; trc_cookie_storage=taboola%2520global%253Auser-id%3Db66f3e30-dd4d-429c-9e40-06a34397d236-tuct33c4874; viewed="30458408"; push_noty_num=0; push_doumail_num=0; acw_tc=276082a315682121587344277ee055770855b2bea693ff05b07c81730ed783; UM_distinctid=16d33142cca8c-08976d3fc7c8f8-3c604504-1fa400-16d33142ccb240; _ga=GA1.2.2035684642.1554454926; ct=y; Hm_lvt_16a14f3002af32bf3a75dfe352478639=1569908316; Hm_lvt_19fc7b106453f97b6a84d64302f21a04=1569911303; __utmc=30149280; __utmz=30149280.1570621121.45.25.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmc=223695111; dbcl2="71620970:jcZXXwrCkKA"; ck=7vt0; __utma=30149280.2035684642.1554454926.1570623176.1570625859.47; __utmt=1; __utmb=30149280.2.10.1570625859; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1570625862%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_ses.100001.4cf6=*; __utma=223695111.1088930519.1555121082.1570623176.1570625862.35; __utmb=223695111.0.10.1570625862; __utmz=223695111.1570625862.35.28.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; monitor_count=18; _pk_id.100001.4cf6=4188bfc644de230d.1555121081.35.1570625864.1570623932.',
+    # "Cookie": 'Cookie:bid=R_NjGlmHzLc; ll="108289"; __utmv=30149280.7162; douban-profile-remind=1; __guid=223695111.2801261442483869700.1555121081221.0579; __yadk_uid=su0b7K9g36n02VtPodU6bxjSmrqQNeVi; _vwo_uuid_v2=DA75957D9968A846830D702056248817A|624640ba21d7d6658c71f663d419f44e; douban-fav-remind=1; gr_user_id=d3b07b2b-f556-4601-b7a2-37f2cbe0d6fc; __gads=ID=af43163fe1df4ca9:T=1557648109:S=ALNI_MYSMPOl2Hkvn60cUHrt3jzyod7RSg; trc_cookie_storage=taboola%2520global%253Auser-id%3Db66f3e30-dd4d-429c-9e40-06a34397d236-tuct33c4874; viewed="30458408"; push_noty_num=0; push_doumail_num=0; acw_tc=276082a315682121587344277ee055770855b2bea693ff05b07c81730ed783; UM_distinctid=16d33142cca8c-08976d3fc7c8f8-3c604504-1fa400-16d33142ccb240; _ga=GA1.2.2035684642.1554454926; ct=y; Hm_lvt_16a14f3002af32bf3a75dfe352478639=1569908316; Hm_lvt_19fc7b106453f97b6a84d64302f21a04=1569911303; __utmc=30149280; __utmz=30149280.1570621121.45.25.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; __utmc=223695111; dbcl2="71620970:jcZXXwrCkKA"; ck=7vt0; __utma=30149280.2035684642.1554454926.1570623176.1570625859.47; __utmt=1; __utmb=30149280.2.10.1570625859; _pk_ref.100001.4cf6=%5B%22%22%2C%22%22%2C1570625862%2C%22https%3A%2F%2Fwww.douban.com%2F%22%5D; _pk_ses.100001.4cf6=*; __utma=223695111.1088930519.1555121082.1570623176.1570625862.35; __utmb=223695111.0.10.1570625862; __utmz=223695111.1570625862.35.28.utmcsr=douban.com|utmccn=(referral)|utmcmd=referral|utmcct=/; monitor_count=18; _pk_id.100001.4cf6=4188bfc644de230d.1555121081.35.1570625864.1570623932.',
 }
-
-result = defaultdict(list)
 
 
 def output_help(filename):
     """控制台输出帮助说明"""
+
     print('标准命令 -> python {filename} -t <剧集类型> -r <评分> -v <评价人数> -n <筛选几个>'.format(filename=filename))
     print('-t 参数说明：us美剧,gb英剧,kr韩剧,jp日剧,cn国产剧,jpa日本动画,df纪录片')
     print('一个例子【搜索美剧】 -> python {filename} -t us -r 8.5 -v 10000 -n 3'.format(filename=filename))
@@ -66,7 +64,7 @@ def get_vote(url):
     return votes
 
 
-def get_popular_tv_series(**kwargs):
+def get_popular_tv_series(result, **kwargs):
     """获取热门电视剧集"""
 
     tag = kwargs.pop('tag')
@@ -107,6 +105,7 @@ def get_popular_tv_series(**kwargs):
 
 def conver_tags(abbr_tags):
     """剧集类型英中转换"""
+
     try:
         arg = [TAG_CONVERSION_DICT[abbr_tag] for abbr_tag in abbr_tags]
     except KeyError as e:
@@ -123,10 +122,10 @@ def get_standard_args():
         -v 评价人数（vote）
         -n 筛选几个（nums）
     """
+    data = {}
+    filename = sys.argv[0]
+    console_args = sys.argv[1:]
     try:
-        data = {}
-        filename = sys.argv[0]
-        console_args = sys.argv[1:]
         opts, other_args = getopt.getopt(console_args, "t:r:v:n:", ["help"])
         # print(f'opts:{opts}')
         # print(f'args:{other_args}')
@@ -140,10 +139,9 @@ def get_standard_args():
             output_help(filename)
             sys.exit()
 
-        # 剧集类型英中转换
         if opt == '-t':
             abbr_tags = arg.split(',')
-            arg = conver_tags(abbr_tags)
+            arg = conver_tags(abbr_tags)  # 剧集类型英中转换
         data.update({opt: arg})
     return data
 
@@ -174,12 +172,13 @@ def main():
         print('Warning!!! 参数有误', e)
         sys.exit(2)
 
+    result = defaultdict(list)
     # 按不同剧集类型抓取
     for tag in tags:
         print('\n')
         print('开始筛选【{tag}】...'.format(tag=tag))
 
-        get_popular_tv_series(tag=tag, rate=rate, vote=vote, nums=nums)
+        get_popular_tv_series(result, tag=tag, rate=rate, vote=vote, nums=nums)
 
     # 输出结果
     output_result(result)
