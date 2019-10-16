552616b307293de2b8439d14ddaa294fa1ba3e2e
yueyeKIDL <ace1412kid@163.com>
2019-10-16 14:13:58 +0800

    douban重构

---
+++ douban/linshi.py
@@ -0,0 +1,9 @@
+"""
+AUTHOR:yeuyeKIDL
+DATE:2019/10/16 -10:39
+"""
+import numpy as np
+
+base_rate = np.mean([])
+
+print(base_rate)
--- glorious/settings.py
+++ glorious/settings.py
@@ -87,6 +87,7 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"max_connections": 100}
         }
     }
 }
