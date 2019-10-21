77af8e90b1089e634e9ab02fa77d2499716e8362
yueyeKIDL <ace1412kid@163.com>
2019-10-21 17:22:24 +0800

    剧集筛选数据格式优化

--- douban/linshi.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logger = logging.getLogger('douban')
-
-try:
-    1 / 0
-except Exception as e:
-    logger.exception(e)
-print('gogogo')
--- douban/views.py
+++ douban/views.py
@@ -1,6 +1,8 @@
+import decimal
 import logging
 import time
 from collections import defaultdict
+from decimal import Decimal
 
 import numpy as np
 import requests
@@ -19,22 +21,28 @@ logger = LimitLogger(logger=logging.getLogger('douban'))
 def get_vote(url, headers):
     """获取评价人数"""
 
-    time.sleep(0.1)
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, 'lxml')
     vote = soup.select('#interest_sectl > div > div.rating_self.clearfix > div > div.rating_sum > a > span')[0].text
+    time.sleep(0.1)
     return int(vote)
 
 
-def get_base_rate(subjects):
-    """获取基准评分 - 当前页面所有剧集的平均分"""
+def get_base_data(tag_objs):
+    """
+    获取基准数据 - 去掉一个最大值和最小值，并求平均
+        base_rate - 基准评分
+        base_vote - 基准投票数
+    """
 
-    rates = []
-    for subject in subjects:
-        rate = float(subject['rate'])
-        rates.append(rate)
-    base_rate = np.mean(rates)
-    return base_rate
+    rates = [tag_obj['rate'] for tag_obj in tag_objs]
+    votes = [tag_obj['vote'] for tag_obj in tag_objs]
+
+    base_rate = np.mean(sorted(rates)[1:-1])
+    base_vote = np.mean(sorted(votes)[1:-1]) * 0.75
+    base_rate = Decimal(base_rate).quantize(Decimal('0.00'), rounding=decimal.ROUND_HALF_UP)
+    base_vote = Decimal(base_vote).quantize(Decimal('0.00'), rounding=decimal.ROUND_HALF_UP)
+    return base_rate, base_vote
 
 
 # url解码
@@ -48,7 +56,6 @@ def grab_douban_tv():
 
     # 初始化剧集类型和投票基数
     tags = ['美剧', '英剧', '韩剧', '日剧', '国产剧', '港剧', '日本动画', '综艺', '纪录片']
-    base_vote = 10000
 
     headers = {
         "Host": "movie.douban.com",
@@ -58,44 +65,74 @@ def grab_douban_tv():
     }
 
     url = 'https://movie.douban.com/j/search_subjects?'
-    douban_tv_data = []
+    douban_tv_data = defaultdict(list)
 
     for tag in tags:
         print('开始筛选 【{}】...'.format(tag))
         payload = dict(type='tv', tag=tag, sort='recommend', page_limit=20, page_start=0)
         r = requests.get(url, headers=headers, params=payload)
         subjects = r.json()['subjects']
-        base_rate = get_base_rate(subjects)
-        if not base_rate:
-            break
-        titles = defaultdict(list)
-        rates = defaultdict(list)
-        votes = defaultdict(list)
-        for subject in subjects:
 
+        # 收集剧集信息
+        for subject in subjects:
             subject_title = subject['title']
             subject_rate = float(subject['rate'])
             subject_url = subject['url']
+            subject_vote = get_vote(subject_url, headers)
+            douban_tv_data[tag].append(
+                {
+                    'title': subject_title,
+                    'rate': subject_rate,
+                    'vote': subject_vote,
+                    'url': subject_url
+                }
+            )
+            print('【{}】信息已收集...'.format(subject_title))
 
-            # 大于基准分，抓取其vote
-            if subject_rate > base_rate:
-                subject_vote = get_vote(subject_url, headers)
+        # 筛选达标剧集
+        tag_objs = douban_tv_data[tag]
+        base_rate, base_vote = get_base_data(tag_objs)
 
-                # 保存该tag类型的标题、评分、投票数
-                if subject_vote > base_vote:
-                    titles['titles'].append(subject_title)
-                    rates['rates'].append(subject_rate)
-                    votes['votes'].append(subject_vote)
+        tmp = []
+        for tag_obj in tag_objs:
+            rate = tag_obj['rate']
+            vote = tag_obj['vote']
+            if rate > base_rate and vote > base_vote:
+                tmp.append(tag_obj)
 
-                    print('【{}】入选...'.format(subject_title))
+        douban_tv_data[tag] = tmp
 
-        temp_data = {'tag': tag, **titles, **rates, **votes}
+        print(22222, douban_tv_data)
 
-        # 未筛选出剧集,不保存数据
-        if len(temp_data) > 1:
-            douban_tv_data.append(temp_data)
-    cache.delete_pattern("douban_tv_data")
+    print('\n筛选完毕...')
+    douban_tv_data = convert_data_format(douban_tv_data)
+    # cache.delete_pattern("douban_tv_data")
+    cache.delete("douban_tv_data")
     cache.set("douban_tv_data", douban_tv_data, timeout=7 * 24 * 60 * 60)
+
+
+def convert_data_format(douban_tv_data):
+    """转换数据格式 - 提供满足页面展示的数据格式"""
+
+    result = []
+
+    for tag, tag_objs in douban_tv_data.items():
+        tmp = defaultdict(list)
+        tmp['tag'] = tag
+
+        for tag_obj in tag_objs:
+            title = tag_obj['title']
+            rate = tag_obj['rate']
+            vote = tag_obj['vote']
+            url = tag_obj['url']
+
+            tmp['titles'].append(title)
+            tmp['rates'].append(rate)
+            tmp['votes'].append(vote)
+            tmp['urls'].append(url)
+
+        result.append(tmp)
+    return result
 
 
 # 定时任务调度
@@ -108,7 +145,7 @@ def update_douban_tv_cache(request):
     """手动更新剧集缓存 - 被动更新"""
     if request.user.is_superuser:
         grab_douban_tv()
-        return HttpResponse('更新完毕！')
+        return HttpResponse('剧集更新完毕！')
     return HttpResponseForbidden()
 
 
