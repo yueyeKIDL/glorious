fefcec4bdfc6078715ee0ea04b17256ec81aa069
yueyeKIDL <ace1412kid@163.com>
2019-10-22 10:50:23 +0800

    剧集重构

--- douban/views.py
+++ douban/views.py
@@ -17,6 +17,13 @@ from utils.limit_logger import LimitLogger
 # 日志
 logger = LimitLogger(logger=logging.getLogger('douban'))
 
+headers = {
+    "Host": "movie.douban.com",
+    "Referer": "https://movie.douban.com/",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+}
+
 
 def get_vote(url, headers):
     """获取评价人数"""
@@ -44,71 +51,59 @@ def get_base_data(tag_objs):
     base_vote = Decimal(base_vote).quantize(Decimal('0.00'), rounding=decimal.ROUND_HALF_UP)
     return base_rate, base_vote
 
+    # url解码
+    # from urllib import parse
+    #
+    # s = '%E7%83%AD%E9%97%A8'
+    # print(parse.unquote(s))
 
-# url解码
-# from urllib import parse
-#
-# s = '%E7%83%AD%E9%97%A8'
-# print(parse.unquote(s))
 
-def grab_douban_tv():
-    """抓取热门剧集"""
+def get_tag_page_json(tag):
+    """获取当前剧集页返回json信息"""
 
-    # 初始化剧集类型和投票基数
-    tags = ['美剧', '英剧', '韩剧', '日剧', '国产剧', '港剧', '日本动画', '综艺', '纪录片']
-
-    headers = {
-        "Host": "movie.douban.com",
-        "Referer": "https://movie.douban.com/",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
-    }
-
+    print('开始筛选 【{}】...'.format(tag))
     url = 'https://movie.douban.com/j/search_subjects?'
-    douban_tv_data = defaultdict(list)
+    payload = dict(type='tv', tag=tag, sort='recommend', page_limit=20, page_start=0)
+    r = requests.get(url, headers=headers, params=payload)
+    subjects = r.json()['subjects']
+    return subjects
 
-    for tag in tags:
-        print('开始筛选 【{}】...'.format(tag))
-        payload = dict(type='tv', tag=tag, sort='recommend', page_limit=20, page_start=0)
-        r = requests.get(url, headers=headers, params=payload)
-        subjects = r.json()['subjects']
 
-        # 收集剧集信息
-        for subject in subjects:
-            subject_title = subject['title']
-            subject_rate = float(subject['rate'])
-            subject_url = subject['url']
-            subject_vote = get_vote(subject_url, headers)
-            douban_tv_data[tag].append(
-                {
-                    'title': subject_title,
-                    'rate': subject_rate,
-                    'vote': subject_vote,
-                    'url': subject_url
-                }
-            )
-            print('【{}】信息已收集...'.format(subject_title))
+def collect_tv_data(douban_tv_data, tag, subjects):
+    """收集剧集信息"""
 
-        # 筛选达标剧集
-        tag_objs = douban_tv_data[tag]
-        base_rate, base_vote = get_base_data(tag_objs)
+    for subject in subjects:
+        subject_title = subject['title']
+        subject_rate = float(subject['rate'])
+        subject_url = subject['url']
+        subject_vote = get_vote(subject_url, headers)
+        douban_tv_data[tag].append(
+            {
+                'title': subject_title,
+                'rate': subject_rate,
+                'vote': subject_vote,
+                'url': subject_url
+            }
+        )
+        print('【{}】信息已收集...'.format(subject_title))
+    return douban_tv_data
 
-        tmp = []
-        for tag_obj in tag_objs:
-            rate = tag_obj['rate']
-            vote = tag_obj['vote']
-            if rate > base_rate and vote > base_vote:
-                tmp.append(tag_obj)
 
-        douban_tv_data[tag] = tmp
+def filter_tv_series(douban_tv_data, tag):
+    """筛选达标剧集"""
 
-        print(22222, douban_tv_data)
+    tag_objs = douban_tv_data[tag]
+    base_rate, base_vote = get_base_data(tag_objs)
 
-    print('\n筛选完毕...')
-    douban_tv_data = convert_data_format(douban_tv_data)
-    # cache.delete_pattern("douban_tv_data")
-    cache.delete("douban_tv_data")
-    cache.set("douban_tv_data", douban_tv_data, timeout=7 * 24 * 60 * 60)
+    tmp = []
+    for tag_obj in tag_objs:
+        rate = tag_obj['rate']
+        vote = tag_obj['vote']
+        if rate > base_rate and vote > base_vote:
+            tmp.append(tag_obj)
+
+    douban_tv_data[tag] = tmp
+    return douban_tv_data
 
 
 def convert_data_format(douban_tv_data):
@@ -133,6 +128,33 @@ def convert_data_format(douban_tv_data):
 
         result.append(tmp)
     return result
+
+
+def grab_douban_tv():
+    """抓取热门剧集"""
+
+    # 初始化剧集类型和投票基数
+    tags = ['美剧', '英剧', '韩剧', '日剧', '国产剧', '港剧', '日本动画', '综艺', '纪录片']
+    douban_tv_data = defaultdict(list)
+
+    for tag in tags:
+        # 获取当前剧集页返回json信息
+        subjects = get_tag_page_json(tag)
+
+        # 收集剧集信息
+        douban_tv_data = collect_tv_data(douban_tv_data, tag, subjects)
+
+        # 筛选达标剧集
+        douban_tv_data = filter_tv_series(douban_tv_data, tag)
+
+    print('\n筛选完毕...')
+    douban_tv_data = convert_data_format(douban_tv_data)
+
+    # redis cache
+    # cache.delete_pattern("douban_tv_data")
+
+    cache.delete("douban_tv_data")
+    cache.set("douban_tv_data", douban_tv_data, timeout=7 * 24 * 60 * 60)
 
 
 # 定时任务调度
